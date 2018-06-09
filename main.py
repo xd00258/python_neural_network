1b37a1337bf3386af30f50d0dcb8fb63a4773696
xd00258 <victoriquevuong@gmail.com>
2018-06-09 15:05:06 -0400

    training with random random seed

--- main.py
+++ main.py
@@ -18,12 +18,24 @@ X = np.array([
 y = np.array([[0,0,1,1]]).T
 
 #seed numbers for always same random sequence (for practice)
-np.random.seed(1)
+#np.random.seed(1)
 
-#weight matrix of dimension 3x1 initialized at random
+#weight matrix of dimension 3x1 initialized at random, mean of 0 between -1 and 1
 syn0 = 2*np.random.random((3,1)) - 1
 
+#begin training
+for iter in range(10000):
 
+    #forward propagation
+    l0 = X         #layer 0 is our input dataset with 4 training examples
+    l1 = nonlin(np.dot(l0,syn0))    #layer 1 is our prediction step
 
+    #calculate miss/loss
+    l1_error = y - l1
+    l1_delta = l1_error * nonlin(l1,True)
 
-print(syn0)
+    #update weights
+    syn0 += np.dot(l0.T, l1_delta)
+
+print("Output after training: ")
+print(l1)
