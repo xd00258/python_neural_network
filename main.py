b29f439ca60c2e1f00b4877761ce201fd8ac603b
xd00258 <victoriquevuong@gmail.com>
2018-06-09 14:41:37 -0400

    input/output datasets and weight matrix

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ def nonlin(x, deriv=False):
         return x*(1-x)
     return 1/(1+np.exp(-x))
 
+#input dataset
+X = np.array([
+    [0,0,1],    #one row is one training example
+    [0,1,1],    #columns are input node
+    [1,0,1],
+    [1,1,1]
+    ])
+
+#output dataset
+y = np.array([[0,0,1,1]]).T
+
+#seed numbers for always same random sequence (for practice)
+np.random.seed(1)
+
+#weight matrix of dimension 3x1 initialized at random
+syn0 = 2*np.random.random((3,1)) - 1
 
 
-#print (nonlin(5,True))
+
+
+print(syn0)
